5d30a0f29a186c2d728ab0cdbf9e9bfab9c8086e
ErickFSilva <erick_ferreira@outlook.com>
2024-12-02 16:43:53 -0300

    formacao_logica_de_Programacao, 02/12/2024(tarde)

--- formacao_logica_de_Programacao/secao02_introducao_a_logica_de_programacao/4_trabalhando_com_variaveis/aulas.js
+++ formacao_logica_de_Programacao/secao02_introducao_a_logica_de_programacao/4_trabalhando_com_variaveis/aulas.js
@@ -1,3 +1,8 @@
+/**
+ * - VARIÁVEIS E CONSTANTES PRA QUE TE QUERO
+ */
+
+
 /* Declarando variáveis na prática: */
 console.log("Digite o nome do seu jogador.");
 let nickname = "Erick";
@@ -18,7 +23,6 @@ console.log(notificacao + ": você foi derrotado por um líder.")
 console.log("\n");
 
 
-
 /* Colocando seu conhecimento em prática */
 let poteCafe = "Café pilão";
 let poteAcucar = "Açucar cristal";
@@ -26,9 +30,37 @@ let poteBiscoito = "Biscoito maizena";
 const mensagemDaVovo = "Na cozinha da vovó hoje tem: ";
 
 console.log(mensagemDaVovo + poteCafe + " - " + poteAcucar + " - " + poteBiscoito);
-console.log("\n");
 
 poteCafe = "Café 3 corações";
 
 console.log(mensagemDaVovo + poteCafe + " - " + poteAcucar + " - " + poteBiscoito);
 console.log("\n");
+
+
+/**
+ * - TIPOS DE VARIÁVEIS
+ * 
+ * 	String: São variáveis que armazenam texto;
+ * 	Number: São variáveis que armazenam números;
+ * 	Boolean: Sâo variáveis lógicas, que armazenam o valor de true ou false;
+ */
+
+
+/* Entendendo Strings, Numbers e Booleanos */
+let nomePokemon = "pikachu";
+let pokemonSexo = "M";
+
+let nivelPokemon = 20;
+let pontosDeVidaPokemon = 45;
+
+let selecionavel = false;
+
+
+/* Exercício de fixação */
+let nomePessoa = "Josicreide do Pythinho";
+let idade = 115;
+let numeroTelefone = "(11) 98471-3388";
+let tipoTelefone = "Celular";
+let cpf = "312.122.312-45";
+let endereco = "Rua dos Limoeiros, 75, Jagua Tirí";
+let recebePensao = true;
